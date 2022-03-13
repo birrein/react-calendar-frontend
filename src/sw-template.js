58df302a1b32b2e208dae41edac01ba2e919d4aa
Manuel Marín <birrein@gmail.com>
2022-03-12 22:16:39 -0300

    Optimized Service Worker and added Online/offline status

--- src/sw-template.js
+++ src/sw-template.js
@@ -5,35 +5,35 @@ importScripts(
 
 workbox.loadModule('workbox-background-sync');
 
+// eslint-disable-next-line no-restricted-globals
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 const { registerRoute } = workbox.routing;
 const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
 const { BackgroundSyncPlugin } = workbox.backgroundSync;
 
-registerRoute(
-  new RegExp(
-    'http://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'
-  ),
-  new CacheFirst()
-);
+const cacheNetworkFirst = ['/api/auth/renew', '/api/events'];
 
-registerRoute(
-  new RegExp(
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'
-  ),
-  new CacheFirst()
-);
+registerRoute(({ request, url }) => {
+  if (cacheNetworkFirst.includes(url.pathname)) return true;
+  return false;
+}, new NetworkFirst());
 
-registerRoute(
-  new RegExp('http://localhost:4000/api/auth/renew'),
-  new NetworkFirst()
-);
+// Reference:
+// registerRoute(
+//   new RegExp('http://localhost:4000/api/auth/renew'),
+//   new NetworkFirst()
+// );
 
-registerRoute(
-  new RegExp('http://localhost:4000/api/events'),
-  new NetworkFirst()
-);
+const cacheCacheFirst = [
+  'http://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css',
+];
+
+registerRoute(({ request, url }) => {
+  if (cacheCacheFirst.includes(url.href)) return true;
+  return false;
+}, new CacheFirst());
 
 // Offline sync
 
